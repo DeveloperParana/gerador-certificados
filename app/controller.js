201003058209c32cb5b08435eba44799f08a523e
guuibayer <guiilherme.bayer@gmail.com>
2016-03-29 13:10:39 -0300

    styles to certificate

--- app/controller.js
+++ app/controller.js
@@ -1,26 +1,29 @@
-(function() {
-    'use strict'
-    
-    angular
-        .module('app')
-        .controller('AppController', AppController)
-        
-    AppController.$inject = ['$http','$location','$routeParams']
-    
-    function AppController($http,$location,$routeParams) {
-        var ctrl = this
-        $http.get('data/2-meetup-mga.json').then(function(response) {
-            ctrl.users = response.data
-            if ($routeParams.userId) {
-                angular.forEach(ctrl.users, function(user) {
-                    if (user['Identificação do usuário'] == $routeParams.userId) {
-                        ctrl.user = user
-                    }
-                })
+;(function() {
+  'use strict'
+  
+  angular
+    .module('app')
+    .controller('AppController', AppController)
+      
+  AppController.$inject = ['$http','$location','$routeParams']
+  
+  function AppController($http, $location, $routeParams) {
+    var ctrl = this;
+
+    $http.get('data/2-meetup-mga.json')
+      .then(function(response) {
+        ctrl.users = response.data
+        if ($routeParams.userId) {
+          angular.forEach(ctrl.users, function(user) {
+            if (user['Identificação do usuário'] == $routeParams.userId) {
+              ctrl.user = user;
             }
-        })
-        ctrl.certificate = function(user) {
-            $location.path('certificate/' + user['Identificação do usuário'])
-        }
-    }
-})()
+          });
+        };
+      });
+
+      ctrl.certificate = function(user) {
+        $location.path('certificate/' + user['Identificação do usuário']);
+      };
+  };
+})();
--- app/module.js
+++ app/module.js
@@ -1,6 +1,6 @@
-(function() {
-    'use strict'
-    
-    angular
-        .module('app', ['ngRoute'])
-})()
+;(function() {
+  'use strict'
+  
+  angular
+    .module('app', ['ngRoute'])
+})();
