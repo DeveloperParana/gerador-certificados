6e0e55ffea57a4d1fe9d0778fa4a171ec420c60e
Guilherme Siquinelli <guiseek@gmail.com>
2016-03-29 00:26:53 -0300

    Start Certificados Project

---
+++ app/controller.js
@@ -0,0 +1,26 @@
+(function() {
+    'use strict'
+    
+    angular
+        .module('app')
+        .controller('AppController', AppController)
+        
+    AppController.$inject = ['$http','$location','$routeParams']
+    
+    function AppController($http,$location,$routeParams) {
+        var ctrl = this
+        $http.get('data/2-meetup-mga.json').then(function(response) {
+            ctrl.users = response.data
+            if ($routeParams.userId) {
+                angular.forEach(ctrl.users, function(user) {
+                    if (user['Identificação do usuário'] == $routeParams.userId) {
+                        ctrl.user = user
+                    }
+                })
+            }
+        })
+        ctrl.certificate = function(user) {
+            $location.path('certificate/' + user['Identificação do usuário'])
+        }
+    }
+})()
---
+++ app/routes.js
@@ -0,0 +1,22 @@
+(function() {
+    'use strict';
+    
+    angular
+        .module('app')
+        .config(Config)
+        
+    function Config($routeProvider) {
+        $routeProvider
+            .when('/', {
+                templateUrl: 'app/search.html',
+                controller: 'AppController',
+                controllerAs: 'ctrl'
+            })
+            .when('/certificate/:userId', {
+                templateUrl: 'app/certificate.html',
+                controller: 'AppController',
+                controllerAs: 'ctrl'
+            })
+    }
+        
+})()
